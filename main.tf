a6679c0fd944694da5ad1f683f1d47e9627e4fd6
fareednn <faridnn4@gmail.com>
2021-09-28 14:49:58 +0530

    terraformtest.txt

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  # Configuration options
+  region     = "ap-south-1"
+ 
+  
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c1a7f89451184c8b"
+  instance_type = "t2.micro"
+  
+
+  tags = {
+    Name = "TerraformInstance"
+  }
+}
